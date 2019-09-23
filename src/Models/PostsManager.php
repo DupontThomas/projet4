6b42786937694f5bb14c37f927df4800d183be8c
DUPONT <thomas.dupont1@laposte.net>
2019-09-23 16:37:57 +0200

    Create PostController

--- src/Models/PostsManager.php
+++ src/Models/PostsManager.php
@@ -7,12 +7,12 @@ use Config\DbConnection;
 
 class PostsManager extends DbConnection {
 
-    function getLastChapter {
+    function getLastPost {
         $db = $this->dbConnect();
         $req = $db->prepare('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts ORDER BY id DESC LIMIT 1');
         $req->execute();
-        $lastChapter = $req->fetch();
-        return $lastChapter;
+        $lastPost = $req->fetch();
+        return $lastPost;
     }
 
 }
---
+++ src/controllers/PostsController.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\Controllers;
+
+use App\Models\PostsManager;
+
+
+class PostsController
+{
+    function lastPost()
+    {
+        $postManager = new PostsManager();
+        $lastPost = $postManager->getLastPost();
+        return $lastPost;
+    }
+
+}
